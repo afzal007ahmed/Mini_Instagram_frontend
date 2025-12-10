58514dee2af9e9c1656e14a79fd3b5dd91a17c29
Afzal <afzalahmeddc@gmail.com>
2025-12-10 17:40:27 +0530

    changes for socket.io

--- src/socket.js
+++ src/socket.js
@@ -2,10 +2,10 @@ import { io  } from "socket.io-client";
 
 
 const socket = io(import.meta.env.SOCKET_URL , {
-    withCredentials : true 
+    withCredentials : true ,
+    autoConnect : true 
 }) ;
 
-console.log( socket.connected ) ;
 
 
 export default socket ; 
