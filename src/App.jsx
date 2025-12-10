6cf475ecaecf7ba6de1a7a9529f7a5557ee32cb9
Afzal <afzalahmeddc@gmail.com>
2025-12-10 17:11:08 +0530

    adding socket.io

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import useUserDetails from "./hooks/useUserDetails";
 import RoutesManager from "./Routes/RoutesManager";
 import { useEffect } from "react";
+import socket from "./socket";
+
 
 function App() {
   const { userDetails } = useUserDetails();
+  const user = useSelector(( state ) => state.userReducer ) ;
+
+
+  useEffect(() => {
+    if( user?.data?.id ) {
+      socket.emit('register' , {
+        userId : user.data.id 
+      })
+    }
+  } , [user.data])
+
 
   useEffect(() => {
     userDetails();
