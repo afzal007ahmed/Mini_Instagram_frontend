05d702bc3c99f27bf5f411baa9f36a2232f72da6
Afzal <afzalahmeddc@gmail.com>
2025-12-10 17:48:24 +0530

    changes for socket.io

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,10 @@ import { io  } from "socket.io-client";
 
 const socket = io(import.meta.env.SOCKET_URL , {
     withCredentials : true ,
-    autoConnect : true 
+    autoConnect : true ,
+    reconnection : true ,
+    reconnectionAttempts : Infinity ,
+    reconnectionDelay : 2000
 }) ;
 
 if( !socket.connected ) {
