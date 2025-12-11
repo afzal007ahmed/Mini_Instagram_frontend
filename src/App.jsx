ef4965062c5cc6f40364848d1027cc55cea1f353
Afzal <afzalahmeddc@gmail.com>
2025-12-11 14:31:29 +0530

    changes in websocket logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import useUserDetails from "./hooks/useUserDetails";
 import RoutesManager from "./Routes/RoutesManager";
 import { useEffect } from "react";
 import socket from "./socket";
 import { toast } from "sonner";
+import useFeed from "./hooks/useFeed";
+import useHandleLikeDislike from "./hooks/useHandleLikeDislike";
 
 function App() {
   const { userDetails } = useUserDetails();
+  const dispatch = useDispatch();
+  const { getPostsReactions, getReactionCount } =
+    useHandleLikeDislike();
+  const { postsFeed } = useFeed();
   const user = useSelector((state) => state.userReducer);
-  function socketNotify( title , message) {
+  function socketNotify(title, message) {
     toast(<p className="text-md font-bold">{title}</p>, {
       description: (
         <p className="font-bold text-green-600 text-md">{message}</p>
@@ -33,13 +39,15 @@ function App() {
     });
 
     socket.on("register-response", ({ message }) => {
-      socketNotify( "Connection status" , message)
+      socketNotify("Connection status", message);
     });
-  
-    socket.on('create-post-response' , ({message}) => {
-      socketNotify( "Update" ,  message ) ;
-    })
 
+    socket.on("create-post-response", ({ message }) => {
+      postsFeed() ;
+      getPostsReactions();
+      getReactionCount() ;
+      socketNotify("Update", message);
+    });
   }, []);
 
   return (
